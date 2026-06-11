71ed3d7b9a531b7f3df9e2ee54cf63b1c5a5d9d4
Captain-Kanak <kanakroy835@gmail.com>
2026-06-11 15:23:17 +0600

    successfully split code

--- cmd/main.go
+++ cmd/main.go
@@ -3,35 +3,12 @@ package main
 import (
 	"ticket-booking-system/internal/config"
 	"ticket-booking-system/internal/server"
-
-	"github.com/go-playground/validator/v10"
-	"github.com/labstack/echo/v5"
-	"github.com/labstack/echo/v5/middleware"
 )
-
-type CustomValidator struct {
-	validator *validator.Validate
-}
-
-func (cv *CustomValidator) Validate(i any) error {
-	if err := cv.validator.Struct(i); err != nil {
-		return echo.ErrBadRequest.Wrap(err)
-	}
-
-	return nil
-}
 
 func main() {
 	cfg := config.LoadEnv()
 
 	db := config.ConnectDatabase(cfg)
 
-	e := echo.New()
-
-	// * echo middleware & validator
-	e.Use(middleware.RequestLogger())
-	e.Validator = &CustomValidator{validator: validator.New()}
-
-	// * start server
-	server.StartServer(e, cfg, db)
+	server.StartServer(cfg, db)
 }
--- internal/server/http.go
+++ internal/server/http.go
@@ -7,11 +7,31 @@ import (
 	"ticket-booking-system/internal/httpresponse"
 	"ticket-booking-system/internal/user"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 	"gorm.io/gorm"
 )
 
-func StartServer(e *echo.Echo, cfg *config.EnvConfig, db *gorm.DB) {
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.ErrBadRequest.Wrap(err)
+	}
+
+	return nil
+}
+
+func StartServer(cfg *config.EnvConfig, db *gorm.DB) {
+	e := echo.New()
+
+	// * echo middleware & validator
+	e.Use(middleware.RequestLogger())
+	e.Validator = &CustomValidator{validator: validator.New()}
+
 	// * basics api routes
 	e.GET("/", func(c *echo.Context) error {
 		return c.JSON(http.StatusOK, httpresponse.Response{
