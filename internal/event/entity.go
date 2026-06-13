99973b7faffe2de6b5dbacaba10c6661beebd70e
Captain-Kanak <kanakroy835@gmail.com>
2026-06-13 18:41:24 +0600

    successfully done event model

--- internal/event/entity.go
+++ internal/event/entity.go
@@ -7,24 +7,27 @@ import (
 	"gorm.io/gorm"
 )
 
-type Venue struct {
-	ID          uuid.UUID      `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	Title       string         `json:"title" gorm:"type:varchar(255);not null"`
-	Description string         `json:"description" gorm:"type:text"`
-	City        string         `json:"city" gorm:"type:varchar(255);not null"`
-	Capacity    int            `json:"capacity" gorm:"type:int;not null"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
+type Event struct {
+	ID               uuid.UUID      `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Title            string         `json:"title" gorm:"type:varchar(255);not null"`
+	Description      string         `json:"description" gorm:"type:text;not null"`
+	Location         string         `json:"location" gorm:"type:text;not null"`
+	StartDate        time.Time      `json:"start_date" gorm:"type:timestamp;not null"`
+	TotalTickets     int            `json:"total_tickets" gorm:"type:int;not null"`
+	AvailableTickets int            `json:"available_tickets" gorm:"type:int;not null"`
+	Price            float64        `json:"price" gorm:"type:float;not null"`
+	CreatedAt        time.Time      `json:"created_at"`
+	UpdatedAt        time.Time      `json:"updated_at"`
+	DeletedAt        gorm.DeletedAt `json:"deleted_at"`
 }
 
-type Event struct {
-	ID          uint
-	Title       string
-	Description string
-	Category    string
-	VenueID     uint
-	Venue       Venue
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-}
+// type Venue struct {
+// 	ID          uuid.UUID      `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+// 	Title       string         `json:"title" gorm:"type:varchar(255);not null"`
+// 	Description string         `json:"description" gorm:"type:text"`
+// 	City        string         `json:"city" gorm:"type:varchar(255);not null"`
+// 	Capacity    int            `json:"capacity" gorm:"type:int;not null"`
+// 	CreatedAt   time.Time      `json:"created_at"`
+// 	UpdatedAt   time.Time      `json:"updated_at"`
+// 	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
+// }
